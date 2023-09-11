553a91dcbcddfabbab84b4a30fb4f0f39d37b26e
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2023-09-10 23:41:59 -0400

    added back arros for filter pane

--- src/components/filterproducts/FilterPane.jsx
+++ src/components/filterproducts/FilterPane.jsx
@@ -2,14 +2,15 @@ import { useEffect, useState } from 'react'
 import './FilterPane.scss'
 import axios from 'axios'
 import { Link, useParams } from 'react-router-dom';
-
+import {ReactComponent as BackIcon} from '../../assets/icons/arrow-sm-left-svgrepo-com.svg'
 
 function FilterPane({data,URL}){
     const [category, setCategory] = useState([]);
     const [subcategory, setSubCategory] = useState([]);
     const [selectCategory, setSelectCategory] = useState([]);
     const [selectSubcategory, setSelectSubCategory] = useState([]);
-    const {category_1} = useParams();
+
+    const {category_1, category_2, category_3, product_name} = useParams();
     useEffect(()=>{
         setCategory(getUnique(data,'category_2'))
         setSubCategory(getUnique(data,'category_3'))
@@ -44,7 +45,7 @@ function FilterPane({data,URL}){
                         {    category.map((i)=>{
                             return(
                                 <div className="filterpane__category2">
-                                    <Link to={`/collection/${category_1}`}><h3>{i.category_2.replaceAll("_"," | ").toUpperCase()}</h3></Link>
+                                    <Link to={`/collection/${category_1}`} >{category_3?<BackIcon className="back-icon" style={{ width: '40px', height: '40px' }} />:""}<h3>{i.category_2.replaceAll("_"," | ").toUpperCase()}</h3></Link>
                                     {subcategory.filter((p)=>p.category_2 === i.category_2).map((i)=>{
                                     return(
                                         <div className="filterpane__category3">
